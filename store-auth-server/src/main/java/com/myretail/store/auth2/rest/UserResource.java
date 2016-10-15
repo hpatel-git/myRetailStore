8d010f6bc7b02eea64cba2e4590b5c9e1377f09d
Hardik Patel <hardikpatel@LEECO-2343M.local>
2016-10-15 11:35:51 -0700

    Removing un necessary functionality

--- store-auth-server/src/main/java/com/myretail/store/auth2/rest/UserResource.java
+++ store-auth-server/src/main/java/com/myretail/store/auth2/rest/UserResource.java
@@ -5,8 +5,6 @@ package com.myretail.store.auth2.rest;
 
 import java.security.Principal;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -15,8 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.myretail.store.auth2.model.UserProfile;
 
 /**
  * The Class UserResource.
@@ -43,32 +39,10 @@ public class UserResource {
 	 */
 	@RequestMapping(method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_UTF8_VALUE })
 	@ResponseBody
-	public Principal user(Principal user) {
+	public Principal getUserInfo(Principal user) {
+		LOGGER.debug("UserResource.getUserInfo");
 		return user;
 	}
 
-	/**
-	 * Gets the user locale.
-	 *
-	 * @param request
-	 *            the request
-	 * @param principal
-	 *            the principal
-	 * @return the user locale
-	 * @throws Exception
-	 *             the exception
-	 */
-	@RequestMapping(value = "/locale", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_UTF8_VALUE })
-	@ResponseBody public UserProfile getUserLocale(HttpServletRequest request, Principal principal) {
-		UserProfile profile = new UserProfile();
-		String defaultLocale = "en_US";
-		if (request.getSession().getAttribute("defaultLocale") != null) {
-			defaultLocale = (String) request.getSession().getAttribute("defaultLocale");
-		}
-		profile.setUserName(principal.getName());
-		profile.setLocale(defaultLocale);
-		LOGGER.debug("UserResource.getUserLocal : " + defaultLocale);
-		return profile;
-	}
 
 }
